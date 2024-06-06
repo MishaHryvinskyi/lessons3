d3baceb4b896f8c108e6bb10f6b830ec3cfa91a6
Misha <hryvinskyim@gmail.com>
2024-06-06 21:47:45 +0300

    create

---
+++ lessons3/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+
+  <input type="number" v-model="counter">
+  <CounterVue :counter="counter"/>
+
+</template>
+
+<script>
+
+import { defineComponent, ref } from 'vue';
+import  CounterVue  from '@/components/CounterVue.vue';
+
+
+export default defineComponent ({
+  components: { CounterVue },
+  setup () {
+
+    const counter = ref(0);
+
+
+    return {
+      counter,
+    }
+  }
+  
+})
+</script>
+
+<style>
+
+</style>
---
+++ lessons3/src/components/CounterVue.vue
@@ -0,0 +1,24 @@
+<template>
+  <h2>Child component: {{  counter  }}</h2>
+</template>
+
+<script>
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  props: {
+    counter: Number,
+  },
+  setup () {
+
+
+    return {
+
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
