2b0e274b7c54f59bbfb09fc7e81f5fd6b581f181
Misha <hryvinskyim@gmail.com>
2024-06-06 22:12:27 +0300

    end

--- lessons3/src/App.vue
+++ lessons3/src/App.vue
@@ -1,25 +1,53 @@
 <template>
 
-  <input type="number" v-model="counter">
-  <CounterVue :counter="counter"/>
+ <div class="main-block">
+  <label for="modalTitle">
+    <input id="modalTitle" type="text" v-model="modalTitle"/>
+  </label>
+  <label for="modalMessage">
+    <input id="modalMessage" type="text" v-model="modalMessage"/>
+  </label>
+  
+  <button @click="showModal">Show modal</button>
+ </div>
+
+ <ModalWindow v-if="isModalOpen" :modal-title="modalTitle" :modal-message="modalMessage" @close="closeModal"/>
 
 </template>
 
 <script>
 
 import { defineComponent, ref } from 'vue';
-import  CounterVue  from '@/components/CounterVue.vue';
+import ModalWindow from '@/components/ModalWindow.vue';
 
 
 export default defineComponent ({
-  components: { CounterVue },
+  components: {ModalWindow},
   setup () {
 
-    const counter = ref(0);
+    const isModalOpen = ref(false);
+    const modalTitle = ref('');
+    const modalMessage = ref('');
+    
+    function showModal() {
+      isModalOpen.value = true;
+      modalTitle.value = "";
+      modalMessage.value = "";
+    }
 
+    function closeModal() {
+      isModalOpen.value = false;
+      modalTitle.value = "";
+      modalMessage.value = "";
+    }
+    
 
     return {
-      counter,
+      isModalOpen,
+      modalTitle,
+      modalMessage,
+      showModal,
+      closeModal
     }
   }
   
---
+++ lessons3/src/components/ModalWindow.vue
@@ -0,0 +1,39 @@
+<template>
+  
+  <div class="modal">
+    <div class="content">
+      <h3>{{ modalTitle }}</h3>
+      <p>{{ modalMessage }}</p>
+      <button @click="closeModal">Close</button>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  props: {
+    modalMessage: String,
+    modalTitle: String
+  },
+
+  emits: ['close'],
+
+  setup (props, {emit}) {
+
+    function closeModal() {
+      emit('close')
+    }
+
+    return {
+      closeModal
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
